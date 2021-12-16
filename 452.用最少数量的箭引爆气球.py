1f19c252da498dc3ee9a5c412103962b67624a51
zqp111 <zhangbo91100@163.com>
2021-12-16 14:58:21 +0800

    greedy

--- 452.用最少数量的箭引爆气球.py
+++ 452.用最少数量的箭引爆气球.py
@@ -9,15 +9,20 @@ class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         if len(points) == 0:
             return 0
-        points.sort(key=lambda a: a[0])
-        print(points)
+        points.sort()
+        # print(points)
         nums = 1
+        # left = points[0][0]
         right = points[0][1]
         for l in points[1:]:
-            if l[0] > right:
+            if l[0] <= right:
+                # left =l[0]
+                right = min(l[1], right)
+            elif l[0] > right:
                 nums += 1
+                # left = l[0]
                 right = l[1]
-        print(nums)
+        # print(nums)
         return nums
 
 # @lc code=end
