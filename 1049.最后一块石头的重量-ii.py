6ce734cf66bbe50a5d96187908494f8240cbe86e
zqp111 <zhangbo91100@163.com>
2021-12-17 21:26:35 +0800

    bag

--- 1049.最后一块石头的重量-ii.py
+++ 1049.最后一块石头的重量-ii.py
@@ -7,5 +7,14 @@
 # @lc code=start
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
+        numsSum = sum(stones)
+        targetNum = numsSum // 2
+
+        re = [0]*(targetNum + 1)
+        for i in range(len(stones)):
+            for j in range(targetNum, stones[i]-1, -1):
+                re[j] = max(re[j], re[j-stones[i]]+stones[i])
+        return numsSum - 2*re[-1]
+
 # @lc code=end
 
