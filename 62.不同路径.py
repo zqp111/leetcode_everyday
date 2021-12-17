5255ebff3ebd03a6869ac8e8db6a8b0ba20d9a1f
zqp111 <zhangbo91100@163.com>
2021-12-17 21:50:02 +0800

    dp

--- 62.不同路径.py
+++ 62.不同路径.py
@@ -7,6 +7,11 @@
 # @lc code=start
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        
+        dp = [[1 if i==0 or j==0 else 0 for i in range(n)] for j in range(m)]
+        # print(dp)
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
 # @lc code=end
 
