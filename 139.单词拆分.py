20bb7fb0d0f7651eb46a7fbba07429167e3d663f
zqp111 <zhangbo91100@163.com>
2021-12-18 22:17:40 +0800

    dp

--- 139.单词拆分.py
+++ 139.单词拆分.py
@@ -7,7 +7,12 @@
 # @lc code=start
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-
-
+        dp = [0] * (len(s)+1)
+        dp[0] = 1
+        for i in range(len(s)):
+            for j in range(i+1, len(s)+1):
+                if s[i:j] in wordDict:
+                    dp[j] += dp[i]
+        return True if dp[-1] != 0 else False
 # @lc code=end
 
