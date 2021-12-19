154eb7bfb523a982cafef770e588de6b86dc4e0d
zqp111 <zhangbo91100@163.com>
2021-12-19 17:44:23 +0800

    dp

---
+++ 718.最长重复子数组.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=718 lang=python3
+#
+# [718] 最长重复子数组
+#
+
+# @lc code=start
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1)+1)]
+        result = 0
+        for i in range(1, len(nums1)+1):
+            for j in range(1, len(nums2)+1):
+                if nums1[i-1] == nums2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                # else:
+                #     dp[i][j] = 0
+                result = max(dp[i][j], result)
+        return result
+# @lc code=end
+
