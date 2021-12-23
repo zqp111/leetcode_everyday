c2cf8b05d09495bedba19ddfa43059dc380c6188
zqp111 <zhangbo91100@163.com>
2021-12-23 22:19:12 +0800

    hash

---
+++ 454.四数相加-ii.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=454 lang=python3
+#
+# [454] 四数相加 II
+#
+
+# @lc code=start
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        hashmap = dict()
+        for n1 in nums1:
+            for n2 in nums2:
+                if n1 + n2 in hashmap:
+                    hashmap[n1+n2] += 1
+                else:
+                    hashmap[n1+n2] = 1
+        
+        # if the -(a+b) exists in nums3 and nums4, we shall add the count
+        count = 0
+        for n3 in nums3:
+            for n4 in nums4:
+                key = - n3 - n4
+                if key in hashmap:
+                    count += hashmap[key]
+        return count
+# @lc code=end
+
