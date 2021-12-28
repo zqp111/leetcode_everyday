25447973e8debd5bca2488b0aef09b7d8d87886f
zqp111 <zhangbo91100@163.com>
2021-12-28 14:50:10 +0800

    NEW: greedy binary search

--- 300.最长递增子序列.py
+++ 300.最长递增子序列.py
@@ -7,15 +7,32 @@
 # @lc code=start
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        dp = []
-        for i in range(len(nums)):
-            dp.append(1)
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-        return max(dp)            
+        ## dp
+        # if not nums:
+        #     return 0
+        # dp = []
+        # for i in range(len(nums)):
+        #     dp.append(1)
+        #     for j in range(i):
+        #         if nums[i] > nums[j]:
+        #             dp[i] = max(dp[i], dp[j] + 1)
+        # return max(dp)            
+
+
+        ## greedy search
+        
+        if not nums: return 0
+        minEnd = [nums[0]]
+        maxLen = 1
+        for i in range(1, len(nums)):
+            index = bisect_left(minEnd, nums[i])
+            if index == len(minEnd):
+                minEnd.append(nums[i])
+                maxLen += 1
+            else:
+                minEnd[index] = nums[i]
+        return maxLen
+            
 
 # @lc code=end
 
