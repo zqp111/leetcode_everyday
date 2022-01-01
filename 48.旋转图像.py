1c6a5d804f294c8a475e272094e21498648ef70d
zqp111 <zhangbo91100@163.com>
2022-01-01 21:16:55 +0800

    array

---
+++ 48.旋转图像.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=48 lang=python3
+#
+# [48] 旋转图像
+#
+
+# @lc code=start
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        l, m = divmod(n, 2)
+        if m:
+            l += 1
+        for i in range(l):
+            for j in range(n//2):
+                tmp = matrix[n-j-1][i]
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1]
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1]
+                matrix[j][n-i-1] = matrix[i][j]
+                matrix[i][j] = tmp
+        
+# @lc code=end
+
