4717958b54edf2c5897a70b59786df122632eb41
zqp111 <zhangbo91100@163.com>
2022-01-01 21:16:49 +0800

    dp

--- 70.爬楼梯.py
+++ 70.爬楼梯.py
@@ -7,17 +7,23 @@
 # @lc code=start
 class Solution:
     def climbStairs(self, n: int) -> int:
-        res = [0 for _ in range(n+1)]
-        def get_n(n, res):
-            if res[n] >0:
-                return res[n]
-            if n <=2:
-                res[n] = n
-                return res[n]
-            else:
-                res[n] = get_n(n-1, res) + get_n(n-2, res)
-                return res[n]
-        get_n(n, res)
-        return res[n]
+        # res = [0 for _ in range(n+1)]
+        # def get_n(n, res):
+        #     if res[n] >0:
+        #         return res[n]
+        #     if n <=2:
+        #         res[n] = n
+        #         return res[n]
+        #     else:
+        #         res[n] = get_n(n-1, res) + get_n(n-2, res)
+        #         return res[n]
+        # get_n(n, res)
+        # return res[n]
+        dp = [0] * (n+1)
+        dp[-1] = 1
+        dp[-2] = 1
+        for i in range(n-2, -1, -1):
+            dp[i] = dp[i+1]+ dp[i+2]
+        return dp[0]
 # @lc code=end
 
