454e30c7d517799ca8a528e556a11dc67cde7f68
zqp111 <zhangbo91100@163.com>
2022-01-03 16:14:58 +0800

    dp

--- 322.零钱兑换.py
+++ 322.零钱兑换.py
@@ -7,5 +7,13 @@
 # @lc code=start
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        for cost in range(1, amount+1):
+            for coin in coins:
+                if cost < coin:
+                    continue
+                dp[cost] = min(dp[cost], 1+dp[cost-coin])
+        return dp[-1] if dp[-1] != float('inf') else -1
 # @lc code=end
 
