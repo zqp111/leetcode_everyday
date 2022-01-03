631512706e02973caac5971bbc15369b435ef338
zqp111 <zhangbo91100@163.com>
2022-01-03 20:17:57 +0800

    stack

--- 456.132-模式.py
+++ 456.132-模式.py
@@ -7,7 +7,19 @@
 # @lc code=start
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        a = nums.argsort()
-                
+        n = len(nums)
+        candidate_k = [nums[n - 1]]
+        max_k = float("-inf")
+
+        for i in range(n - 2, -1, -1):
+            if nums[i] < max_k:
+                return True
+            while candidate_k and nums[i] > candidate_k[-1]:
+                max_k = candidate_k[-1]
+                candidate_k.pop()
+            if nums[i] > max_k:
+                candidate_k.append(nums[i])
+
+        return False  
 # @lc code=end
 
