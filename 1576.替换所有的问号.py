dd37f43ce113c1b5e633bf8ecce2b58c595b73b0
zqp111 <zhangbo91100@163.com>
2022-01-05 21:01:55 +0800

    string

---
+++ 1576.替换所有的问号.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=1576 lang=python3
+#
+# [1576] 替换所有的问号
+#
+
+# @lc code=start
+class Solution:
+    def modifyString(self, s: str) -> str:
+        res = list(s)
+        n = len(res)
+        for i in range(n):
+            if res[i] == '?':
+                for b in "abc":
+                    if not (i > 0 and res[i - 1] == b or i < n - 1 and res[i + 1] == b):
+                        res[i] = b
+                        break
+        return ''.join(res)
+# @lc code=end
+
