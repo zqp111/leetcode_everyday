363cf665021b1b9f7aec4dc9fc8a118305b71e12
zqp111 <zhangbo91100@163.com>
2022-01-06 21:39:19 +0800

    backtracking

---
+++ 22.括号生成.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=22 lang=python3
+#
+# [22] 括号生成
+#
+
+# @lc code=start
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        def backtrack(S, left, right):
+            if len(S) == 2 * n:
+                ans.append(''.join(S))
+                return
+            if left < n:
+                S.append('(')
+                backtrack(S, left+1, right)
+                S.pop()
+            if right < left:
+                S.append(')')
+                backtrack(S, left, right+1)
+                S.pop()
+
+        backtrack([], 0, 0)
+        return ans
+
+# @lc code=end
+
