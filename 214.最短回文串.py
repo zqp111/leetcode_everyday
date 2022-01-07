6c9073ab39911b13fbd3499572cdb9de3bf8cb43
zqp111 <zhangbo91100@163.com>
2022-01-07 19:51:21 +0800

    string

--- 214.最短回文串.py
+++ 214.最短回文串.py
@@ -7,5 +7,16 @@
 # @lc code=start
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
+        dp = [[0] * len(s) for _ in range(len(s))]
+        maxLen = 0
+        for i in range(len(s)-1, -1, -1):
+            dp[i][i] = 1
+            for j in range(i+1, len(s)):
+                if s[i] == s[j] and dp[i+1][j-1]: dp[i][j] = dp[i+1][j-1] + 2
+                maxLen = max(maxLen, j-i+1)
+        return maxLen 
+
+
+
 # @lc code=end
 
