f93a223ddd59ab501386182d40eceba59409bc55
zqp111 <zhangbo91100@163.com>
2022-01-08 21:26:45 +0800

    string

---
+++ 43.字符串相乘.py
@@ -0,0 +1,59 @@
+#
+# @lc app=leetcode.cn id=43 lang=python3
+#
+# [43] 字符串相乘
+#
+
+# @lc code=start
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        result = '0'
+        for i in range(len(num2)-1, -1, -1):
+            tmp = '0'*(len(num2)-i-1)
+            c = 0
+            for j in range(len(num1)-1, -1, -1):
+                cur = int(num2[i]) * int(num1[j]) + c
+                c, cur = divmod(cur, 10)
+                tmp = str(cur) + tmp
+            if c: tmp = str(c) + tmp
+            # print(tmp)
+            result = self.addStrings(result, tmp)
+        while len(result) > 1:
+            if result[0] == '0': result = result[1:]
+            else: break
+        return result
+
+    def addStrings(self, num1: str, num2: str) -> str:
+        num1Index, num2Index = len(num1)-1, len(num2)-1
+
+        re = ''
+
+        c = 0
+
+        while num1Index >= 0 and num2Index >= 0:
+            n = int(num1[num1Index]) + int(num2[num2Index]) + c
+            c, n = divmod(n, 10)
+
+            re = str(n) + re
+            num1Index -= 1
+            num2Index -= 1
+
+        if num1Index < 0: # 第一个字符串结束
+            while num2Index >= 0:
+                n = int(num2[num2Index]) + c
+                c, n = divmod(n, 10)
+
+                re = str(n) + re
+                num2Index -= 1
+        else:
+            while num1Index >= 0:
+                n = int(num1[num1Index]) + c
+                c, n = divmod(n, 10)
+
+                re = str(n) + re
+                num1Index -= 1
+        if c:
+            re = str(c) + re
+        return re
+# @lc code=end
+
