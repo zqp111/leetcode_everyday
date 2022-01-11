1654f9dc77a492a5477f5759e81d6a08608814d2
zqp111 <zhangbo91100@163.com>
2022-01-11 17:37:39 +0800

    linked List

--- 25.k-个一组翻转链表.py
+++ 25.k-个一组翻转链表.py
@@ -12,5 +12,33 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        
+        newHead = ListNode(0, head)
+        cur = head
+        prevNode = newHead
+        count = 0
+        while cur:
+            count += 1
+            # print(cur, count)
+            if count == k:
+                count = 0
+                prevNode = self.reverse(prevNode, cur.next)
+                cur = prevNode.next
+            else:
+                cur = cur.next
+        return newHead.next
+
+    def reverse(self, prevNode, nextNode):
+        cur = prevNode.next
+        while cur.next is not nextNode:
+            tmp = cur.next.next
+            cur.next.next = prevNode.next
+            prevNode.next = cur.next
+            cur.next = tmp
+        # print(cur.val)
+        return cur
+        
+
+
 # @lc code=end
 
