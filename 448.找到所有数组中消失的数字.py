66b8d4fa20d1a4166fdf27f951a22c97c5045481
zqp111 <zhangbo91100@163.com>
2022-01-11 23:06:23 +0800

    hash

---
+++ 448.找到所有数组中消失的数字.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=448 lang=python3
+#
+# [448] 找到所有数组中消失的数字
+#
+
+# @lc code=start
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for num in nums:
+            x = (num - 1) % n
+            nums[x] += n
+        
+        ret = [i + 1 for i, num in enumerate(nums) if num <= n]
+        return ret
+
+# @lc code=end
+
