a2353395e4663dacdd7b4743fc37450b9f72312b
zqp111 <zhangbo91100@163.com>
2022-01-12 17:19:45 +0800

    dp

---
+++ 312.戳气球.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=312 lang=python3
+#
+# [312] 戳气球
+#
+
+# @lc code=start
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        n = len(nums)
+        rec = [[0] * (n + 2) for _ in range(n + 2)]
+        val = [1] + nums + [1]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 2, n + 2):
+                for k in range(i + 1, j):
+                    total = val[i] * val[k] * val[j]
+                    total += rec[i][k] + rec[k][j]
+                    rec[i][j] = max(rec[i][j], total)
+        
+        return rec[0][n + 1]
+
+# @lc code=end
+
