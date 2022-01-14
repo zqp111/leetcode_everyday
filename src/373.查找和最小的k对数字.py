63cf204936b3188e2002ffa864d5ae5ff38359de
zqp111 <zhangbo91100@163.com>
2022-01-14 10:56:10 +0800

    array

---
+++ src/373.查找和最小的k对数字.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=373 lang=python3
+#
+# [373] 查找和最小的K对数字
+#
+
+# @lc code=start
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        # result = []
+        m, n = len(nums1), len(nums2)
+        ans = []
+        pq = [(nums1[i] + nums2[0], i, 0) for i in range(min(k, m))]
+        while pq and len(ans) < k:
+            _, i, j = heappop(pq)
+            ans.append([nums1[i], nums2[j]])
+            if j + 1 < n:
+                heappush(pq, (nums1[i] + nums2[j + 1], i, j + 1))
+        return ans
+
+# @lc code=end
+
