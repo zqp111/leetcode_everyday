78fc9fafc24710071009ffd584fb49784f8c6307
zqp111 <zhangbo91100@163.com>
2022-01-17 21:50:56 +0800

    dp

---
+++ src/740.删除并获得点数.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=740 lang=python3
+#
+# [740] 删除并获得点数
+#
+
+# @lc code=start
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        maxVal = max(nums)
+        total = [0] * (maxVal + 1)
+        for val in nums:
+            total[val] += val
+        
+        def rob(nums: List[int]) -> int:
+            size = len(nums)
+            first, second = nums[0], max(nums[0], nums[1])
+            for i in range(2, size):
+                first, second = second, max(first + nums[i], second)
+            return second
+        
+        return rob(total)
+# @lc code=end
+
