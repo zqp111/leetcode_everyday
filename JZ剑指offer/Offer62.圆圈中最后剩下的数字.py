6c3f412c77f3e493574fe0524bf9e86f0e2e8e48
zqp111 <zhangbo91100@163.com>
2022-01-23 23:02:20 +0800

    math

---
+++ JZ剑指offer/Offer62.圆圈中最后剩下的数字.py
@@ -0,0 +1,11 @@
+sys.setrecursionlimit(100000)
+
+def f(n, m):
+    if n == 0:
+        return 0
+    x = f(n - 1, m)
+    return (m + x) % n
+
+class Solution:
+    def lastRemaining(self, n: int, m: int) -> int:
+        return f(n, m)
